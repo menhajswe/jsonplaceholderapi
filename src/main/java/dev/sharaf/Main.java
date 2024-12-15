5ea360e0a88d5643ab00234d5aa28508edd1932c
menhajswe <menhajswe@gmail.com>
2024-12-14 19:02:49 -0500

    deleted none test unused folders.

--- src/main/java/dev/sharaf/Main.java
+++
@@ -1,9 +0,0 @@
-package dev.sharaf;
-
-public class Main {
-
-    // This main method is not used.
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
